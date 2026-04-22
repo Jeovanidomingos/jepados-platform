813fa9104be7e9511bd8b88dd8759a63fc2b3b59
Jeovanidomingos <jeovanydomingos@gmail.com>
2026-04-22 14:11:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,23 +5,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let orders = [];
+
 app.get("/", (req, res) => {
   res.json({ status: "JEPADOS API ONLINE" });
 });
 
 app.get("/machines", (req, res) => {
   res.json([
-    { id: 1, name: "Escavadora CAT 320", status: "AVAILABLE" },
-    { id: 2, name: "Bulldozer D6", status: "IN_USE" }
+    { id: 1, name: "Escavadora CAT 320", status: "DISPONÍVEL" },
+    { id: 2, name: "Bulldozer D6", status: "EM_USO" }
   ]);
 });
 
+app.get("/orders", (req, res) => {
+  res.json(orders);
+});
+
 app.post("/orders", (req, res) => {
-  res.json({
+  const order = {
     id: Date.now(),
     ...req.body,
     status: "PENDING"
-  });
+  };
+
+  orders.push(order);
+  res.json(order);
 });
 
 const PORT = process.env.PORT || 3001;
